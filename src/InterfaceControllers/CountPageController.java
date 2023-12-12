04b8d60c2032b552c547a99063e2524e06aa8840
NadirCianid <den4ik.strelok@gmail.com>
2023-12-13 00:37:19 +0300

    Настроил работу чековой страницы

--- src/InterfaceControllers/CountPageController.java
+++ src/InterfaceControllers/CountPageController.java
@@ -3,6 +3,9 @@ package InterfaceControllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static InterfaceControllers.StartPoint.currentClient;
 import static InterfaceControllers.StartPoint.warehouseController;
 
@@ -27,6 +30,12 @@ public class CountPageController {
     private Label totalMoneyAmountLabel;
 
     public void init() {
+        clientNameLabel.setText("Заказчик: " + currentClient.getFio());
+        clientCompanyLabel.setText("Компания заказчика: " + currentClient.getAgreement().getCompanyName());
+        agreementNumberLabel.setText("Номер договора: " + currentClient.getAgreement().getAgreementNumber());
+        positionCountLabel.setText("Позиций в заказе: " + currentClient.basket.getSelectedPositions().size() + " шт.");
+        totalMoneyAmountLabel.setText("Итого к оплате:  " + currentClient.basket.getTotalSum() + " P.");
+        dateLabel.setText("Дата заказа: " + LocalDate.now().format(DateTimeFormatter.ISO_DATE));
 
         warehouseController.makeOrder(currentClient.basket);
     }
